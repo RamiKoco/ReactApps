25bf942c815877a48b0799e86c5348577957c361
RamiKoco <ramikoco333433@gmail.com>
2023-12-26 21:01:13 +0300

    Workshop 25.12.2023

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import "./App.css";
 import Homepage from "./pages/Homepage/Homepage";
 import About from "./pages/About/About";
 import Navbar from "./components/Navbar/Navbar";
+import ProductDetail from "./pages/Product/ProductDetail";
+import AddProduct from "./pages/Product/AddProduct";
+
 
 function App() {
 	return (
@@ -12,7 +15,10 @@ function App() {
 				<Routes>
 					<Route path="/" element={<Homepage />}></Route>
 					<Route path="/about" element={<About />}></Route>
+					<Route path="/add-product" element={<AddProduct />}></Route>
+					<Route path="/product-detail/:productId" element={<ProductDetail/>}></Route>
 					<Route path="*" element={<p>Not Found</p>}></Route>
+					
 				</Routes>
 			</BrowserRouter>
 		</>
--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import { useDeleteProduct } from "../../pages/Product/useDeleteProduct";
 
 export default function ProductCard(props) {
+
+	
+	  const { isDeleted, handleDeleteClick } = useDeleteProduct(props.product.id);
 	return (
 		<div className="card">
 			<img src={props.product.thumbnail} className="card-img-top" alt="..." />
@@ -9,12 +13,15 @@ export default function ProductCard(props) {
 				<h5 className="card-title">{props.product.title}</h5>
 				<p className="card-text">{props.product.description}</p>
 				<Link
-					to={"/product-detail?id=" + props.product.id}
+					to={`/product-detail/${props.product.id}`}
 					className="btn btn-primary"
 				>
 					Details
 				</Link>
-				<button className="btn btn-danger">Sil</button>
+				<button
+          onClick={() =>handleDeleteClick()} 
+          className="btn btn-danger"
+             >Delete</button>
 			</div>
 		</div>
 	);
