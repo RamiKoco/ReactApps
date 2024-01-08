3adf57f6b2bc805a2fcfc630071f62e7018d8a13
RamiKoco <ramikoco333433@gmail.com>
2024-01-08 12:19:45 +0300

    Redux

--- src/pages/ProductDetail/ProductDetail.tsx
+++ src/pages/ProductDetail/ProductDetail.tsx
@@ -1,7 +1,8 @@
 import React, {useEffect, useState} from "react";
 import {useLocation, useParams} from "react-router-dom";
 import {ProductModel} from "../../models/responses/ProductModel";
-import ProductService from "../../services/ProductService";
+import ProductService from "../../services/ProductService" ;
+
 
 type Props = {};
 
